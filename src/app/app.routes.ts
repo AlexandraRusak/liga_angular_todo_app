81f143567c6116d193eff3838f0fc64cfb8a4d8a
Alexandra Rusak <alexandra.myagkova@gmail.com>
2024-03-26 13:31:53 +0300

    added routing, home page(desc), navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {TodoListComponent} from "./todo-list/todo-list.component";
+import {LoginComponent} from "./login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    // children: [
+    //   {
+    //     path: 'green',
+    //     title: 'green',
+    //     canActivate: [elevatorGuard],
+    //     component: GreenRoomComponent,
+  },
+  {
+    path: 'todo-list',
+    title: 'todo-list',
+    // canActivate: [elevatorGuard],
+    component: TodoListComponent,
+  },
+  {
+    path: 'login',
+    // pathMatch: 'full',
+    component: LoginComponent,
+  }
+];
